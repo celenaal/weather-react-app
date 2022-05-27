c66f7182a455af5351d2ffa426761be0f88aa473
celenaal <101164218+celenaal@users.noreply.github.com>
2022-05-26 21:46:03 -0700

    updated save

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,9 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-  function handleResponse(response){alert(`The weather in New York is ${response.data.main.temp}C` )}
+  function handleResponse(response) {
+    alert(`The weather in New York is ${response.data.main.temp}C`);
+  }
   let apiKey = `9c5166e4799f671ad5f5ea3859d91158`;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
   axios.get(url).then(handleResponse);
